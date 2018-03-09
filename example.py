7c7de3e55ab5b5603e7fecd93b8c5a93c77d5ca8
Dukehan1 <834147061@qq.com>
2018-03-10 00:12:01 +0800

    split

--- example.py
+++ example.py
@@ -1,30 +1,33 @@
 # coding: utf-8
 
 import os
-from sklearn.model_selection import StratifiedKFold
-import numpy as np
-import nltk
+from sklearn.model_selection import KFold
 import pandas as pd
 
-def experiment_svm_dev_set(input_path_training):
+def experiment(input_path_training):
     df = pd.read_csv(input_path_training)
     X = []
     y = []
-    tokenizer = nltk.TreebankWordTokenizer()
     for index, row in df.iterrows():
-        X.append(' '.join(tokenizer.tokenize(row['comment_text'].decode('utf-8'))))
-        y.append(row['toxic'])
-    y = np.asarray(y)
+        X.append(row['comment_text'])
+        y.append([
+            row['toxic'],
+            row['severe_toxic'],
+            row['obscene'],
+            row['threat'],
+            row['insult'],
+            row['identity_hate'],
+        ])
     print "Finish loading data"
 
-    skf = StratifiedKFold(n_splits=10, shuffle=True)
-    X_train, X_test, y_train, y_test = None, None, None, None
-    for train_index, test_index in skf.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        break
+    skf = KFold(n_splits=10, random_state=2333, shuffle=True)
+    i = 1
+    for train_index, dev_index in skf.split(X, y):
+        df_dev = df.ix[dev_index]
+        df_dev.to_csv('train-' + str(i) + '.csv')
+        i += 1
     print "Finish splitting data"
 
 if __name__ == "__main__":
     training_set = os.path.join("train.csv")
-    experiment_svm_dev_set(training_set)
+    experiment(training_set)
